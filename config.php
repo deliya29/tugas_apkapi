152d89302a556ed594b5eea93160c1ce9861dc55
deliya29 <115698421+deliya29@users.noreply.github.com>
2022-12-01 16:27:50 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', 'platform_mobile');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_HOST', 'localhost');
+
+    $kon = mysqli_connect("localhost", "root", "","platform_mobile") or die(mysqli_error());
+?>
+
